aac3b2337bc41fed733586422a9132bba9f6e062
AnimeVerse972 <soibjonovdiyorbek0@gmail.com>
2025-10-01 05:22:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -926,4 +926,4 @@ async def on_startup(dp):
     print("✅ PostgreSQL bazaga ulandi!")
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
